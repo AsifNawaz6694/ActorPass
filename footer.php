402b71e2e5e17508130a3a6010604355d6c703f3
aliaimviz <im.irfanmumtaz21@gmail.com>
2018-02-05 10:55:37 +0500

    home page complete

---
+++ footer.php
@@ -0,0 +1,51 @@
+<footer>
+	<div class="footer_content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>ActorsPass is a learning experience. It is not an audition or employment opportunity. As per CSA regulations, when your class is over, the casting director/casting associate teaching your class will not be taking home nor be given access to your headshot, resume or any other of your promotional materials.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="copy-rights">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p>&copy; COPYRIGHT 2018 BY ACTOR PASS, L.L.C.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="assets/js/jquery-3.1.1.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<!-- Bootstrap -->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- Carousel-min -->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!-- Wow-min-js -->
+<script src="assets/js/wow.min.js"></script>
+<!-- masonry-grid-js -->
+<script src="assets/js/masonry.pkgd.min.js"></script>
+<!-- alertify-js -->
+<script src="assets/js/alertify.min.js"></script>
+<!-- juqeyr custom validatoin plugin-js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+<!-- custom validatoin-js -->
+<script src="assets/js/custom_validatiion.js"></script>
+<!-- mobile-menu-js -->
+<script src="assets/plugins/menu/js/jquery.mmenu.all.js"></script>
+<script src="assets/plugins/menu/js/jquery.mhead.js"></script>
+<!-- Animation JS -->
+<script src="assets/js/modernizr.custom.js"></script>	
+<!-- script-js -->
+<script src="assets/js/script.js"></script>
+<!-- Custom-js -->
+<script src="assets/js/custom.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Actor Pass</title>
+	<!-- Bootstrap -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<!-- FontAwesome -->
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+	<!-- Animate -->
+	<link href="assets/css/animate.css" rel="stylesheet">
+	<!-- Owl Slider -->
+	<link href="assets/css/owl.carousel.css" rel="stylesheet">
+	<!-- Owl Slider Theme -->
+	<link href="assets/css/owl.theme.css" rel="stylesheet">
+	<!--Jquery Validation -->
+	<link href="assets/css/validationEngine.jquery.css" rel="stylesheet">
+	<!--Jquery custom Validation -->
+	<link href="assets/css/custom_validatiion.css" rel="stylesheet">
+	<!--Mobile menu -->
+	<link href="assets/plugins/menu/css/hamburgers.css" rel="stylesheet">
+	<link href="assets/plugins/menu/css/jquery.mmenu.all.css" rel="stylesheet">
+	<link href="assets/plugins/menu/css/jquery.mhead.css" rel="stylesheet">
+	<!-- Animation CSS -->
+	<!-- <link rel="stylesheet" type="text/css" href="assets/css/default.css" />
+	<link rel="stylesheet" type="text/css" href="assets/css/component.css" /> -->
+	<!-- style.css -->
+	<link href="assets/css/style.css" rel="stylesheet">
+	<!-- custom Css Lins -->
+	<link href="assets/css/custom.css" rel="stylesheet">
+	<!-- Responsive -->
+	<link href="assets/css/responsive.css" rel="stylesheet">
+
+	
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+<body>
+	<div id="wrapper">
+		<header class="container">
+			<div class="row">
+				<div class="col-md-3 col-sm-8 col-xs-8">
+					<div class="img-logo"  style="margin-top: 21px;">
+						<img src="assets/images/logo_actor.png" alt="Actor Pass Logo" class="img-responsive">
+					</div>
+				</div>
+
+				<div class="col-sm-4 col-xs-4 only-mobile only-tablet">
+					<a href="#my-menu" class="navbar-toggle only-mobile only-tablet">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+					<div id="my-menu">
+						<ul class="nav navbar-nav">
+							<li>
+								<a href="#">HOME</a>
+							</li>
+							<li>
+								<a href="#">ABOUT US</a>
+							</li>
+							<li>
+								<a href="#">TAKE A CLASS</a>
+							</li>
+							<li>
+								<a href="#">FAQ</a>
+							</li>
+							<li>
+								<a href="#">CONTACT US</a>
+							</li>
+							<li>
+								<a href="#">LOGIN</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-md-7 col-sm-12 col-xs-12">
+					<div class="top-header">
+						<div class="header-contact">
+							<div class="pull-left social">
+								<a href="#"> <i class="fa fa-facebook"></i> </a>
+								<a href="#"> <i class="fa fa-twitter"></i> </a>
+								<a href="#"> <i class="fa fa-instagram"></i> </a>
+							</div>
+							<div class="pull-right contact">
+								<a href="#"> <i class="fa fa-phone"></i>  :  + 212-593-0066</a>
+								<a href="#"> <i class="fa fa-envelope"></i> : info@actorpass.com</a>
+							</div>
+						</div>
+						<div class="menu">
+							<nav>
+								<ul class="navigation only-desktop">
+									<li class="active"><a href="#">HOME</a></li>
+									<li><a href="#">ABOUT US</a></li>
+									<li><a href="#">TAKE A CLASS</a></li>
+									<li><a href="#">FAQ</a></li>
+									<li><a href="#">CONTACT</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-2 only-desktop">
+					<div class="login-button" style="margin-top: 35px;">
+						<a href="#" class="btn btn-login">LOGIN</a>
+					</div>
+				</div>
+			</div>
+		</header>
